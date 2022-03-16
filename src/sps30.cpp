1bcd19975feb02c90168410c430093f244f3f4c6
MikaelHB <mikael.hb@hotmail.com>
2022-03-16 12:48:07 +0100

    fjernet unødvendig kode fra sps30.cpp

--- src/sps30.cpp
+++ src/sps30.cpp
@@ -4,10 +4,6 @@
 // Example arduino sketch, based on 
 // https://github.com/Sensirion/embedded-sps/blob/master/sps30-i2c/sps30_example_usage.c
 
-
-// uncomment the next line to use the serial plotter
-// #define PLOTTER_FORMAT
-
 void setup() {
   int16_t ret;
   uint8_t auto_clean_days = 4;
@@ -42,15 +38,6 @@ void setup() {
   Serial.print("measurements started\n");
 #endif /* PLOTTER_FORMAT */
 
-#ifdef SPS30_LIMITED_I2C_BUFFER_SIZE
-  Serial.print("Your Arduino hardware has a limitation that only\n");
-  Serial.print("  allows reading the mass concentrations. For more\n");
-  Serial.print("  information, please check\n");
-  Serial.print("  https://github.com/Sensirion/arduino-sps#esp8266-partial-legacy-support\n");
-  Serial.print("\n");
-  delay(2000);
-#endif
-
   delay(1000);
 }
 
@@ -78,57 +65,16 @@ void loop() {
   } else {
 
 #ifndef PLOTTER_FORMAT
-    Serial.print("PM  1.0: ");
-    Serial.println(m.mc_1p0);
     Serial.print("PM  2.5: ");
     Serial.println(m.mc_2p5);
-    Serial.print("PM  4.0: ");
-    Serial.println(m.mc_4p0);
     Serial.print("PM 10.0: ");
     Serial.println(m.mc_10p0);
-
-#ifndef SPS30_LIMITED_I2C_BUFFER_SIZE
-    Serial.print("NC  0.5: ");
-    Serial.println(m.nc_0p5);
-    Serial.print("NC  1.0: ");
-    Serial.println(m.nc_1p0);
-    Serial.print("NC  2.5: ");
-    Serial.println(m.nc_2p5);
-    Serial.print("NC  4.0: ");
-    Serial.println(m.nc_4p0);
-    Serial.print("NC 10.0: ");
-    Serial.println(m.nc_10p0);
 
     Serial.print("Typical partical size: ");
     Serial.println(m.typical_particle_size);
 #endif
 
     Serial.println();
-
-#else
-    // since all values include particles smaller than X, if we want to create buckets we 
-    // need to subtract the smaller particle count. 
-    // This will create buckets (all values in micro meters):
-    // - particles        <= 0,5
-    // - particles > 0.5, <= 1
-    // - particles > 1,   <= 2.5
-    // - particles > 2.5, <= 4
-    // - particles > 4,   <= 10
-
-    Serial.print(m.nc_0p5);
-    Serial.print(" ");
-    Serial.print(m.nc_1p0  - m.nc_0p5);
-    Serial.print(" ");
-    Serial.print(m.nc_2p5  - m.nc_1p0);
-    Serial.print(" ");
-    Serial.print(m.nc_4p0  - m.nc_2p5);
-    Serial.print(" ");
-    Serial.print(m.nc_10p0 - m.nc_4p0);
-    Serial.println();
-
-
-#endif /* PLOTTER_FORMAT */
-
   }
 
   delay(1000);
